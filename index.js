b28bb1abf8811be7a56e7249ac993bfbbc2075bc
Ricky Rojas <rickyrojas19@gmail.com>
2019-02-18 02:23:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// add solution here
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+    
+function theBeatlesPlay(musicians, instruments) {
+  for (let i = 0; i < musicians.length; i++) 
+    return musicians[i];
+  
+  for (let i = 0; i < instruments.length; i++) 
+    return instruments[i];
+
+  var newString =  musicians[i] + ' plays ' + instruments[i] + ".";
+  
+  return newString;
+}
