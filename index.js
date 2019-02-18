45b10fc46605393c4bd6279c47738ac8242f38f7
Ricky Rojas <rickyrojas19@gmail.com>
2019-02-18 02:30:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
-const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
-    
 function theBeatlesPlay(musicians, instruments) {
-  for (let i = 0; i < musicians.length; i++) 
-    return musicians[i];
-  
-  for (let i = 0; i < instruments.length; i++) 
-    return instruments[i];
+  let beatlesArray = [];
 
-  var newString =  `${musicians[i]} plays ${instruments[i]}.`;
+  for (let i = 0; i < musicians.length; i ++) {
+    beatlesArray.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return beatlesArray;
+}
+
+ const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+function johnLennonFacts(facts) {
+  let factsArray = [];
+  let i = 0
   
-  return newString;
+  while (i < facts.length) { 
+    factsArray.push(`${facts[i]}!!!`)
+    i++;
+  }
+  return factsArray
+}
+
+function iLoveTheBeatles(number) {
+  let returnArray = [];
+
+  do { 
+    returnArray.push(`I love the Beatles!`);
+    number++
+  } while(number < 15)
+      return returnArray
 }
